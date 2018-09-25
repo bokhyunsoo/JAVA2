0f769196aaf4af006dce94c037aeee3cf02b75c5
bok-PC\bok <bok_0120@naver.com>
2018-09-25 20:27:49 +0900

    클래스와 인스턴스

---
+++ src/classpart/Student.java
@@ -0,0 +1,47 @@
+package classpart;
+
+public class Student {
+	private int studentID; // 학번
+	private String studentName; // 학생이름
+	private int grade; // 학년
+	private String address; // 사는 곳
+	
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [studentID=" + studentID + ", studentName=" + studentName + ", grade=" + grade + ", address=" + address + "]";
+	}
+	
+	public static void main(String[] args) {
+		Student studentAhn = new Student();
+//		studentAhn.studentName = "안명수";
+		studentAhn.setStudentName("안명수");
+		
+		System.out.println(studentAhn.getStudentName());
+		System.out.println(studentAhn.studentName);
+	}
+}
