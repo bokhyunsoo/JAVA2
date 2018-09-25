a27a35a256b0e78140d885db90618b249141c191
bok-PC\bok <bok_0120@naver.com>
2018-09-25 17:56:59 +0900

    기본 연산자

---
+++ src/operator/OperationEx1.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class OperationEx1 {
+
+	public static void main(String[] args) {
+		
+		int mathScore = 90;
+		int engScore = 70;
+		
+		int totalScore = mathScore + engScore;
+		System.out.println(totalScore);
+		
+		double avgScore = totalScore / 2.0;
+		System.out.println(avgScore);
+		/////////////////////////////////////
+		
+		int totalScore1 = 0;
+		int engScore1 = 70;
+		
+		totalScore1 = mathScore + engScore;
+	}
+
+}
