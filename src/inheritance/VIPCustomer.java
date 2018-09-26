b4efe9c25f00b7bc3ae1d609f66f70e1239067ca
bok-PC\bok <bok_0120@naver.com>
2018-09-26 15:37:13 +0900

    하위클래스의 생성 과정

--- src/inheritance/VIPCustomer.java
+++ src/inheritance/VIPCustomer.java
@@ -6,11 +6,21 @@ public class VIPCustomer extends Customer {
 	double saleRatio;
 	
 	public VIPCustomer() {
+		super();
 		this.customerGrade = "VIP";
 		this.bonusRatio = 0.05;
 		this.saleRatio = 0.1;
+		System.out.println("VIPCustomer() 생성자 호출");
 	}
-
+	
+	public VIPCustomer(int customerID, String customerName, int agentID) {
+		super(customerID, customerName);
+		this.customerGrade = "VIP";
+		this.bonusRatio = 0.05;
+		this.saleRatio = 0.1;
+		System.out.println("VIPCustomer(int, string, int) 생성자 호출");
+	}
+	
 	public int getAgentID() {
 		return agentID;
 	}
